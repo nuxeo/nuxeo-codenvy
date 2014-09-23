c316219343d15b5be0793cfac6f3275c3aca131b
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-09-23 12:20:59 +0200

    cosmit

--- nuxeo-codenvy-client/src/main/java/org/nuxeo/codenvy/NuxeoExtension.java
+++ nuxeo-codenvy-client/src/main/java/org/nuxeo/codenvy/NuxeoExtension.java
@@ -2,10 +2,8 @@
  * (C) Copyright 2014 Nuxeo SA (http://nuxeo.com/) and contributors.
  *
  *       All rights reserved. This program and the accompanying materials
- *       are made available under the terms of the GNU Lesser General Public
- *       License
- *       (LGPL) version 2.1 which accompanies this distribution,
- *       and is available at
+ *       are made available under the terms of the GNU Lesser General Public License
+ *       (LGPL) version 2.1 which accompanies this distribution, and is available at
  *       http://www.gnu.org/licenses/lgpl-2.1.html
  *
  *       This library is distributed in the hope that it will be useful,
